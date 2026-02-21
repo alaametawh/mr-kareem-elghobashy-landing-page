5d589a4902119bebec14b170de213b11ba9ca532
alaametawh <183715521+alaametawh@users.noreply.github.com>
2026-02-21 02:56:20 +0200

    feat: navbar animatio and UI/UX tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,19 @@ function toggleTheme() {
     console.log(localStorage.getItem("theme"))
 }
 themeToggleBtn.addEventListener("click", toggleTheme)
+
+// Nav animation
+let tl = gsap.timeline()
+tl.from("#nav", {
+    y:-100,
+    duration:0.8,
+    opacity:0,
+    ease: "power2.out"
+})
+tl.from("#logo, #navs > *", {
+    y:-20,
+    duration:0.5,
+    opacity:0,
+    stagger:0.2,
+    ease:"back.out(1.7)"
+})
